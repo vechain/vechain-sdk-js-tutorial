401464ad668f3f5184cc7f896cc2e7be2f96149e
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-11-04 17:49:22 +0000

    feat: financial math lesson

--- src/1.Hello_World/FinancialMath.ts
+++ src/1.Hello_World/FinancialMath.ts
@@ -1,12 +1,14 @@
 // START-SNIPPET: FinancialMath_1
 // STEP 1: import FixedPointNumber and BigNumber to compare results among the two libraries.
-import {FixedPointNumber} from '@vechain/sdk-core';
-import {BigNumber} from 'bignumber.js';
+import { FixedPointNumber } from '@vechain/sdk-core';
+import { BigNumber } from 'bignumber.js';
 
 // STEP 2: create two equivalent objects from different expression types and check they are equivalent.
 let x = FixedPointNumber.of(123.456789);
 let y = FixedPointNumber.of('123.456789');
-console.log(`FPN value ${x} from number is ${x.isEqual(y) ? '' : 'not'}equal to ${y} from string.`);
+console.log(
+    `FPN value ${x} from number is ${x.isEqual(y) ? '' : 'not'}equal to ${y} from string.`
+);
 
 // STEP 3: cast to a number
 console.log(`Cast FPN value to number is ${x.n}.`);
@@ -20,19 +22,21 @@ console.log(`Cast FPN value to bigint is ${x.bi}.`);
 // STEP 5: compute 1/3 comparing JS number, BigNumber and SDK FixedPointNumber math
 x = FixedPointNumber.of(1);
 y = FixedPointNumber.of(3);
-let r = x.div(y);   // r for 'ratio'.
-console.log(`${x}/${y} => JS = ${x.n / y.n};\tBigNumber = ${BigNumber(x.n).div(y.n)};\tSDK = ${r}`);
+let r = x.div(y); // r for 'ratio'.
+console.log(
+    `${x}/${y} => JS = ${x.n / y.n};\tBigNumber = ${BigNumber(x.n).div(y.n)};\tSDK = ${r}`
+);
 
 // END-SNIPPET: FinancialMath_2
 
 // START-SNIPPET: FinancialMath_3
 // STEP 6: increase the precision to 80 decimal digits.
-let fd = 80;            // Fractional Digits.
-x = x.dp(80);                   // Force x to fd precision, .div function will adapt y automatically
-r = x.div(y);                   // Ratio
-let a = BigNumber(x.n).dp(fd);  // Force to fd precision.
-let b = BigNumber(y.n).dp(fd);  // Force to fd precision
-let q = a.div(b)                // q for 'quotient' synonymous of 'ratio'.
+let fd = 80; // Fractional Digits.
+x = x.dp(80); // Force x to fd precision, .div function will adapt y automatically
+r = x.div(y); // Ratio
+let a = BigNumber(x.n).dp(fd); // Force to fd precision.
+const b = BigNumber(y.n).dp(fd); // Force to fd precision
+const q = a.div(b); // q for 'quotient' synonymous of 'ratio'.
 console.log(`${x}/${y} => BigNumber ${q};\t SDK = ${r}`);
 
 // END-SNIPPET: FinancialMath_3
@@ -40,16 +44,16 @@ console.log(`${x}/${y} => BigNumber ${q};\t SDK = ${r}`);
 // START-SNIPPET: FinancialMath_3
 // STEP 7: compute the squared root of the natural number from 0 to n.
 fd = 20;
-let n = 8
+let n = 8;
 let rows = [];
 for (let i = 0; i <= n; i++) {
     x = FixedPointNumber.of(i).dp(fd).sqrt();
     a = BigNumber(i).dp(fd).sqrt();
     rows.push({
         'JS number': Math.sqrt(i),
-        'BigNumber': `${a}`,
+        BigNumber: `${a}`,
         'SDK FixedPointNumber': `${x}`
-    })
+    });
 }
 console.table(rows);
 
@@ -58,16 +62,16 @@ console.table(rows);
 // START-SNIPPET: FinancialMath_4
 // STEP 8: compute the squared root of the natural number from 0 to n.
 fd = 80;
-n = 8
+n = 8;
 rows = [];
 for (let i = 0; i <= n; i++) {
     x = FixedPointNumber.of(i).dp(fd).sqrt();
     a = BigNumber(i).dp(fd).sqrt();
     rows.push({
         'JS number': Math.sqrt(i),
-        'BigNumber': `${a}`,
+        BigNumber: `${a}`,
         'SDK FixedPointNumber': `${x}`
-    })
+    });
 }
 console.table(rows);
 
@@ -86,11 +90,11 @@ console.log(`0/0 = ${r}`);
 
 // START-SNIPPET: FinancialMath_6
 // STEP 10: compute the simple interest reat for 10,000 currency unit at 15% for one year
-let P = 10000; // 10,000 $
-let R = 0.15; // 15% interest rate
-let N = 1; // interest accrued times per year
-let T = 1; // 1 year of investment time
-let jsA = interestWithNumberType(P, R, N, T);
+const P = 10000; // 10,000 $
+const R = 0.15; // 15% interest rate
+const N = 1; // interest accrued times per year
+const T = 1; // 1 year of investment time
+const jsA = interestWithNumberType(P, R, N, T);
 console.log(
     `JS number            => ${P} at ${R} accrued ${N} per year for ${T} years = ${jsA} `
 );
@@ -130,9 +134,7 @@ function interestWithFixedPointNumberType(
     const _r = FixedPointNumber.of(r);
     const _n = FixedPointNumber.of(n);
     const _t = FixedPointNumber.of(t);
-    return FixedPointNumber.ONE.plus(_r.div(_n))
-        .pow(_t.times(_n))
-        .times(_P);
+    return FixedPointNumber.ONE.plus(_r.div(_n)).pow(_t.times(_n)).times(_P);
 }
 
 function interestWithNumberType(
@@ -145,5 +147,3 @@ function interestWithNumberType(
 }
 
 // END-SNIPPET: FinancialMath_Functions
-
-
